e169d6cb87c8f230f34c4714d12c4912b594cecf
mohseen <msaudagar@gammastack.com>
2023-03-15 12:46:28 +0530

    feat: added user auth backend

--- Musico/app/controllers/application_controller.rb
+++ Musico/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :check_login, except: [:login, :new, :validate]
+    
+    private
+    def check_login
+        if(session[:user_id] == nil)
+            redirect_to login_path
+        end
+    end
 end
