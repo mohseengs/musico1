3ea1fc88cfed1ef21933054ab31fbce4d2eb6084
mohseen <msaudagar@gammastack.com>
2023-03-16 16:35:16 +0530

    database schema
    added languages and category for song
    
    created schema for following feature
    
    created database schema for playlist feature
    
    created database schema for like and history
    
    created database schema for song queues
    
    created database schema for group_sessions
    
    created database schema for notification
    
    added dependenies

---
+++ Musico/db/migrate/20230322122905_create_notifications.rb
@@ -0,0 +1,12 @@
+class CreateNotifications < ActiveRecord::Migration[6.1]
+  def change
+    create_table :notifications do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :notifiable_type, null: false
+      t.integer :notifiable_id, null: false
+      t.integer :status, :default => 0
+
+      t.timestamps
+    end
+  end
+end
