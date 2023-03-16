83c8f5c07e33f179ef50076b8ac3a642ad923f90
mohseen <msaudagar@gammastack.com>
2023-03-16 17:56:45 +0530

    refacter: moved signup code from UsersController to AuthenticationController

--- Musico/app/controllers/authentication_controller.rb
+++ Musico/app/controllers/authentication_controller.rb
@@ -1,5 +1,25 @@
 class AuthenticationController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
   layout "authentication"
+
+  def signup
+    @user = User.new(user_params)
+
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   def login
      
@@ -25,6 +45,16 @@ class AuthenticationController < ApplicationController
       puts "user not exist"
       render :login
     end  
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:name, :email, :password, :dob)
   end
 
 end
--- Musico/config/routes.rb
+++ Musico/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get '/login' => 'authentication#login'
   get '/logout' => 'authentication#logout'
   post '/login' => 'authentication#validate'
-  get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
+  get '/signup' => 'authentication#signup'
+  post '/signup' => 'authentication#create'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
