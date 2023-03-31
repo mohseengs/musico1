e69a88dfddc10b387f95ff65cbc95cb41204cb24
mohseen <msaudagar@gammastack.com>
2023-03-31 14:41:18 +0530

    feat: created playlist feature

---
+++ Musico/app/controllers/playlist_songs_controller.rb
@@ -0,0 +1,37 @@
+class PlaylistSongsController < ApplicationController
+  before_action :set_playlist,  only: [:index , :new, :create, :destroy]
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+    @playlist.songs << Song.where(id: params[:songs])
+        
+    render :index
+  end
+
+  def destroy
+    @playlist.songs.find(params[:id]).destroy! 
+    
+    render :index
+  end
+
+
+  def search_song
+    query = params[:query]
+    @songs = Song.where("name LIKE ?", "%" + query + "%")
+
+    render json: @songs 
+  end
+
+  private
+  def set_playlist
+    @playlist = Playlist.find_by_id(params[:playlist_id])
+  end
+ 
+end
