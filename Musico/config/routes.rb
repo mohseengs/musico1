2a3ff3a94465e38b1c9c15573268ac59d6860b6b
mohseen <msaudagar@gammastack.com>
2023-03-31 14:41:18 +0530

    feat: created playlist feature

--- Musico/config/routes.rb
+++ Musico/config/routes.rb
@@ -7,8 +7,12 @@ Rails.application.routes.draw do
   post '/signup' => 'registrations#create'
   delete '/logout' => 'sessions#logout'
   get 'search_user/:query' => 'songs#search_user', as: :search_user
+  get 'search_song/:query' => 'playlist_songs#search_song', as: :search_song
 
   resources :users
   resources :songs
+  resources :playlists do
+    resources :playlist_songs , only: [:index , :new , :create , :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
